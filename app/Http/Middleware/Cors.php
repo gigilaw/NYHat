17fba1f353df02e76b9951c3f2c207d340b64d66
Gigi Law <gigi.law.oona@gmail.com>
2019-12-18 23:07:49 +0900

    feat: cors api middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,8 +8,8 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-      ->header('Access-Control-Allow-Origin', '*')
-      ->header('Access-Control-Allow-Methods', 'GET, POST')
-      ->header('Access-Control-Allow-Headers', 'Content-Type');
+        ->header('Access-Control-Allow-Origin: *')
+        ->header('Access-Control-Allow-Methods: *')
+        ->header('Access-Control-Allow-Headers: *');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,13 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware(['cors'])->group(function () {
+    Route::get('/users', 'API\UserController@index')->name('users.index');
+    Route::post('{nameCode}/register', 'API\UserController@register')->name('users.register');
 
-Route::get('/users', 'API\UserController@index')->name('users.index');
-Route::post('{nameCode}/register', 'API\UserController@register')->name('users.register');
+    Route::get('/tournaments', 'API\TournamentController@index')->name('tournaments.index');
+    Route::get('/{nameCode}/participants', 'API\TournamentController@participants')->name('tournament.participants');
 
-Route::get('/tournaments', 'API\TournamentController@index')->name('tournaments.index');
-Route::get('/{nameCode}/participants', 'API\TournamentController@participants')->name('tournament.participants');
-
-Route::get('/payments', 'API\PaymentController@index')->name('payments.index');
-Route::post('/payments', 'API\PaymentController@update')->name('payments.update');
+    Route::get('/payments', 'API\PaymentController@index')->name('payments.index');
+    Route::post('/payments', 'API\PaymentController@update')->name('payments.update');
+});
