7030a7827e44ed0559884842022435d935821692
Gigi Law <gigi.law.oona@gmail.com>
2019-12-19 00:57:18 +0900

    testing

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -37,8 +37,9 @@ class UserController extends Controller
      *
      * @throws Exception
      */
-    public function register(StoreUser $request, String $nameCode): ParticipantResource
+    public function register(Request $request, String $nameCode): ParticipantResource
     {
+        dd($request);
         $validatedData = $request->validated();
         $user = User::create($validatedData)->fresh();
         $tournament = Tournament::where('name_code', $nameCode)->first();
