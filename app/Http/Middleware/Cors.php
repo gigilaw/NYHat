6acf200c8f0170c632a15bb54d5a9f8e81817dd9
root <gigi.law.14@gmail.com>
2019-12-19 15:44:15 +0000

    feat: cors update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,7 +8,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-      ->header('Access-Control-Allow-Origin', 'https://www.discanddat.com')
+      ->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', '*')
       ->header('Access-Control-Allow-Headers', '*');
     }
