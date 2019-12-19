e55d5bb3e11c94a60d6f3192108424ec82b6bd1a
root <gigi.law.14@gmail.com>
2019-12-19 03:55:30 +0000

    feat: add unique to email validation

--- app/Http/Requests/StoreUser.php
+++ app/Http/Requests/StoreUser.php
@@ -28,7 +28,7 @@ class StoreUser extends FormRequest
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
             'nick_name' => 'string|max:255',
-            'email' => 'required|email',
+            'email' => 'required|email|unique',
             'age' => 'required|digits:2',
             'height' => 'required|digits:3',
             'gender' => 'required|in:' . implode(',', User::$gender),
